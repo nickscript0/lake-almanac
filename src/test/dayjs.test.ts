c489f095c47f41be7706f7c1148fee1596daa1d5
nickscript0 <nickscript0@users.noreply.github.com>
2025-07-13 08:17:54 -0600

    add original tests back

--- src/test/dayjs.test.ts
+++ src/test/dayjs.test.ts
@@ -81,4 +81,74 @@ describe('dayjs timezone tests', () => {
         expect(diff1).toBe(diff2);
         expect(diff1).toBe(diff3);
     });
+
+    xtest('Github Issue: tz func with valueOf bug', () => {
+        // https://github.com/iamkun/dayjs/issues/1462#issuecomment-886705547
+            
+        /**
+         * Sept 24, 2023: Something's weird in dayjs tz still I think: (This suggestion might be a workaround??? https://github.com/iamkun/dayjs/issues/1805)
+         * This doesn't seem consistent, it treats timestamps differently than date strings
+         *  > dayjs.tz('2021-10-31T13:19:36', 'Europe/Berlin').format()
+            '2021-10-31T13:19:36+01:00'
+            > dayjs.tz('2021-10-31T13:19:36', 'Europe/Berlin').toISOString()
+            '2021-10-31T12:19:36.000Z'
+            > dayjs.tz(ts, 'Europe/Berlin').format()
+            '2021-10-31T13:19:36+01:00'
+            > dayjs.tz(ts, 'Europe/Berlin').toISOString()
+            '2021-10-31T12:19:36.303Z'
+        */
+
+        const ts = 1635682776303;
+        const timezone = 'Europe/Berlin';
+
+        const t1 = dayjs.tz(ts, timezone).toISOString(); //OK
+        const a = dayjs.tz(ts, timezone).add(1, 'day');
+        const t2 = a.toISOString(); //OK
+        const t3 = dayjs.tz(a.valueOf(), timezone).toISOString(); //Incorrect
+
+        // This isn't supposed to fail until t3 per the issue, not sure why it's failing here
+        expect(t1).toBe('2021-10-31T13:19:36.303Z');
+        expect(t2).toBe('2021-11-01T13:19:36.303Z');
+        expect(t3).toBe('2021-11-01T14:19:36.303Z');
+    });
+
+    xtest('Github Issue: Timezone conversion is broken when not using DST Time', () => {
+        /**
+         * I revisited this test on Sept 24, 2023 and don't understand what it was testing.
+         * It seemed to be incorrect before as using https://www.timeanddate.com/worldclock/converter.html?iso=20211130T224200&p1=286&p2=1440&p3=195
+         * I have corrected it, and everything seems to work.
+         */
+    
+        // JUL 13, 2025: THIS DONT PASS IN CALGARY, which makes sense as its starting with local time which is not 3h diff from UTC
+        // These first 3 create a time in local timezone then compare to UTC. These pass fine
+        const t1 = dayjs('2021-10-30').set('hour', 18).set('minute', 42).toISOString();
+        expect(t1).toBe('2021-10-30T21:42:00.000Z');
+    
+        const t2 = dayjs('2021-10-31').set('hour', 18).set('minute', 42).toISOString();
+        expect(t2).toBe('2021-10-31T21:42:00.000Z');
+    
+        const t3 = dayjs('2021-11-30').set('hour', 18).set('minute', 42).toISOString();
+        expect(t3).toBe('2021-11-30T22:42:00.000Z');
+    
+        // These next 3 create a time in local timezone, convert to Paris tz, then compare to UTC. These also pass fine
+        // Because the net result should be the same as the first 3.
+        const t1Paris = dayjs('2021-10-30').set('hour', 18).set('minute', 42).tz('Europe/Paris');
+        expect(t1Paris.toISOString()).toBe('2021-10-30T21:42:00.000Z');
+    
+        const t2Paris = dayjs('2021-10-31').set('hour', 18).set('minute', 42).tz('Europe/Paris');
+        expect(t2Paris.toISOString()).toBe('2021-10-31T21:42:00.000Z');
+    
+        const t3Paris = dayjs('2021-11-30').set('hour', 18).set('minute', 42).tz('Europe/Paris');
+        expect(t3Paris.toISOString()).toBe('2021-11-30T22:42:00.000Z');
+    });
+    
+    test('Github Issue: Incorrect UTC offset when manipulating dayJS object (DST/STD change)', () => {
+        let day = dayjs('2021-11-06').tz('Asia/Tel_Aviv').startOf('day');
+        console.log(day.format('HH:mmZ'));
+    
+        // for (let i = 0; i < 48; i++) {
+        //     day = day.add(1, 'hour');
+        //     console.log(`Add ${i}h`, day.format('HH:mmZ'));
+        // }
+    });
 });
